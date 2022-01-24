19b807191cc5d860b097fb5e239b238fa0fa6630
Ilya <74260376+vip77732888@users.noreply.github.com>
2022-01-24 19:07:38 +0600

    Add files via upload

--- order/js/config.js
+++ order/js/config.js
@@ -1,34 +1,52 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+// var slideIndex = 1;
+// showSlides(slideIndex);
 
-function plusSlide() {
-  showSlides(slideIndex += 1);
+// function plusSlide() {
+//   showSlides(slideIndex += 1);
+// }
+
+// function minusSlide() {
+//   showSlides(slideIndex -= 1);  
+// }
+
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("parag");
+//   var dots = document.getElementsByClassName("noguma-pomoki-kadra");
+//   if (n > slides.length) {
+//   slideIndex = 1
+//   }
+//   if (n < 1) {
+//   slideIndex = slides.length
+//   }
+//   for (i = 0; i < slides.length; i++) {
+//   slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//   dots[i].className = dots[i].className.replace("deystvuyus", "");
+//   }
+//   slides[slideIndex - 1].style.display = "block";
+//   dots[slideIndex - 1].className += " deystvuyus";
+// }
+
+// Accordion
+var details = document.querySelectorAll("details");
+for(i=0;i<details.length;i++) {
+  details[i].addEventListener("toggle", accordion);
 }
-
-function minusSlide() {
-  showSlides(slideIndex -= 1);  
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("parag");
-  var dots = document.getElementsByClassName("noguma-pomoki-kadra");
-  if (n > slides.length) {
-  slideIndex = 1
-  }
-  if (n < 1) {
-  slideIndex = slides.length
-  }
-  for (i = 0; i < slides.length; i++) {
-  slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-  dots[i].className = dots[i].className.replace("deystvuyus", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " deystvuyus";
+function accordion(event) {
+  if (!event.target.open) return;
+    var details = event.target.parentNode.children;
+    for(i=0;i<details.length;i++) {
+      if (details[i].tagName != "DETAILS" || 
+         !details[i].hasAttribute('open') || 
+         event.target == details[i]) {
+         continue;
+      }
+      details[i].removeAttribute("open");
+    }
 }
